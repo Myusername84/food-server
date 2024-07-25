780b23ea48cf5188115785305a470f1380a4c59e
Guts <davithachnazaryan@gmail.com>
2024-07-25 10:27:16 -0700

    final

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://food-client-bay.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "DELETE"],
   })
@@ -76,7 +76,7 @@ app.get(
   passport.authenticate("google", { failureRedirect: "/" }),
   async (req, res) => {
     req.session.user = req.user;
-    res.redirect("http://localhost:3000");
+    res.redirect("https://food-client-bay.vercel.app/");
   }
 );
 
